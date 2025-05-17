77f4608c0a94e5d3b9bfeecfbfd0a54ba2164531
Henry Zhang <hello@zhanghe.dev>
2025-05-17 11:19:07 +0800

    Refactor chat page layout and styling; remove commented-out Chat tab from config

--- llama_park_front/config/pageConfig.js
+++ llama_park_front/config/pageConfig.js
@@ -8,11 +8,23 @@ const tabs = [
     path: "/inventory",
     needLogin: false,
   },
-  {
-    name: "Chat",
-    path: "/chat",
-    needLogin: false,
-  },
+  // {
+  //   name: "Chat",
+  //   path: "/chat",
+  //   needLogin: false,
+  // },
+  // {
+  //   name: "My NFT",
+  //   path: "/my-nft",
+  //   needLogin: true,
+  //   nftNumber: true,
+  // },
+  // {
+  //   name: "My Royalty",
+  //   path: "/my-mint",
+  //   needLogin: true,
+  //   minter: true,
+  // },
 ];
 
 export default tabs;
